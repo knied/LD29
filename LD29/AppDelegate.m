a7dd092f218a67dd71433a5d92dc8ba13c557168
Kristof Niederholtmeyer <kristof.niederholtmeyer@rwth-aachen.de>
2014-04-17 17:12:39 +0200

    Initial Commit

---
+++ LD29/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  LD29
+//
+//  Created by Kristof Niederholtmeyer on 17.04.14.
+//  Copyright (c) 2014 Kristof Niederholtmeyer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ LD29/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  LD29
+//
+//  Created by Kristof Niederholtmeyer on 17.04.14.
+//  Copyright (c) 2014 Kristof Niederholtmeyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
